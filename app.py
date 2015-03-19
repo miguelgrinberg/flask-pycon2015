13c0c45d2ac75b991ff2cb842fa9015859960b5f
Miguel Grinberg <miguelgrinberg50@gmail.com>
2015-03-19 01:03:14 -0500

    flask-wtf forms (v0.8)

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
 #!/usr/bin/env python
 
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, redirect, url_for
+from flask_wtf import Form
+from wtforms.fields import RadioField, SubmitField
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret!'
 questions = ['Is it compiled?', 'Does it run on a VM?']
 guesses = ['Python', 'Java', 'C++']
+
+
+class YesNoQuestionForm(Form):
+    answer = RadioField('Your answer', choices=[('yes', 'Yes'), ('no', 'No')])
+    submit = SubmitField('Submit')
 
 
 @app.route('/')
@@ -14,12 +22,13 @@ def index():
 
 @app.route('/question/<int:id>', methods=['GET', 'POST'])
 def question(id):
-    if request.method == 'POST':
-        if request.form['answer'] == 'yes':
+    form = YesNoQuestionForm()
+    if form.validate_on_submit():
+        if form.answer.data == 'yes':
             return redirect(url_for('question', id=id+1))
         else:
             return redirect(url_for('question', id=id))
-    return render_template('question.html', question=questions[id])
+    return render_template('question.html', question=questions[id], form=form)
 
 
 @app.route('/guess/<int:id>')
