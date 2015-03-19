ded48051db376d6aaa9940fdc2d00035dd33a1a7
Miguel Grinberg <miguelgrinberg50@gmail.com>
2015-03-19 00:46:07 -0500

    links (v0.5)

--- app.py
+++ app.py
@@ -3,12 +3,18 @@
 from flask import Flask, render_template
 
 app = Flask(__name__)
+questions = ['Is it compiled?', 'Does it run on a VM?']
 guesses = ['Python', 'Java', 'C++']
 
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+
+@app.route('/question/<int:id>')
+def question(id):
+    return render_template('question.html', question=questions[id])
 
 
 @app.route('/guess/<int:id>')
