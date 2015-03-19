f5bd2012386fb12c13a77a47588a2fd8c56be0f6
Miguel Grinberg <miguelgrinberg50@gmail.com>
2015-03-19 00:36:46 -0500

    templates (v0.4)

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 guesses = ['Python', 'Java', 'C++']
@@ -8,13 +8,12 @@ guesses = ['Python', 'Java', 'C++']
 
 @app.route('/')
 def index():
-    return '<h1>Guess the Language!</h1>'
+    return render_template('index.html')
 
 
 @app.route('/guess/<int:id>')
 def guess(id):
-    return ('<h1>Guess the Language!</h1>'
-            '<p>My guess: {0}</p>').format(guesses[id])
+    return render_template('guess.html', guess=guesses[id])
 
 
 if __name__ == '__main__':
